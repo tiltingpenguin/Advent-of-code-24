d0f1e3f1eec005ee15de805139e5273653f8b35c
nkrapp <nico.krapp@web.de>
2024-12-04 22:59:01 +0100

    day03-2 solution

--- day03/src/bin/day03-2.rs
+++ day03/src/bin/day03-2.rs
@@ -1,4 +1,20 @@
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::{complete::anychar, streaming},
+    combinator::value,
+    multi::{many1, many_till},
+    sequence::{preceded, separated_pair, terminated},
+    IResult, Parser,
+};
 use std::fs;
+
+#[derive(Debug, Clone, PartialEq)]
+enum Instruction {
+    Pair(u64, u64),
+    True,
+    False,
+}
 
 fn main() {
     let path = "day03-input.txt";
@@ -8,8 +24,50 @@ fn main() {
     println!("{}", result);
 }
 
+fn parse(input: &str) -> IResult<&str, Vec<Instruction>> {
+    many1(many_till(anychar, cond).map(|(_junk, instruction)| instruction))(input)
+}
+
+fn mul(input: &str) -> IResult<&str, Instruction> {
+    let (input, parsed) = preceded(
+        tag("mul("),
+        terminated(
+            separated_pair(streaming::u64, tag(","), streaming::u64),
+            tag(")"),
+        ),
+    )(input)?;
+    return Ok((input, Instruction::Pair(parsed.0, parsed.1)));
+}
+
+fn cond(input: &str) -> IResult<&str, Instruction> {
+    let instruction = alt((
+        value(Instruction::False, tag("don't()")),
+        value(Instruction::True, tag("do()")),
+        mul,
+    ))(input);
+    return instruction;
+}
+
 pub fn process(input: String) -> u64 {
-    todo!("day03 - part 2");
+    let (_rest, instructions) = parse(&input).unwrap();
+    let result = instructions
+        .iter()
+        .fold(
+            (Instruction::True, 0),
+            |(action, acc), instruction| match instruction {
+                Instruction::Pair(a, b) => {
+                    if action == Instruction::True {
+                        (action, acc + a * b)
+                    } else {
+                        (action, acc)
+                    }
+                }
+                Instruction::True => (Instruction::True, acc),
+                Instruction::False => (Instruction::False, acc),
+            },
+        )
+        .1;
+    return result;
 }
 
 #[cfg(test)]
@@ -18,10 +76,11 @@ mod tests {
 
     #[test]
     fn day03_part2_test() {
-        let path = "day03-test.txt";
+        // I got stuck on this so long because I didn't notice the test case for part 2 was
+        // different
+        let path = "day03-2-test.txt";
         let test_input = fs::read_to_string(path).expect("Should be able to read input");
         let result = process(test_input);
-        todo!("haven't put in test result yet")
-        assert_eq!(result, 0);
+        assert_eq!(result, 48);
     }
 }
