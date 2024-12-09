5e0e5009933a9310cc80769f8230cd6874ae43c7
nkrapp <nico.krapp@web.de>
2024-12-09 15:27:57 +0100

    fixed templates

--- template/src/bin/{{project-name}}-2.rs
+++ template/src/bin/{{project-name}}-2.rs
@@ -21,7 +21,7 @@ mod tests {
         let path = "{{project-name}}-test.txt";
         let test_input = fs::read_to_string(path).expect("Should be able to read input");
         let result = process(test_input);
-        todo!("haven't put in test result yet")
+        todo!("haven't put in test result yet");
         assert_eq!(result, 0);
     }
 }
